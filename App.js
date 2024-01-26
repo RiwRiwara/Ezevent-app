3f52e883f1f06067d8f75d524d6f10d80b9dbe89
RiwRiwara <awirut2629@gmail.com>
2024-01-26 18:34:34 +0700

    #initialize mobile - add landing page - setup project structure

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
+
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/Logo.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>EZEVENT</Text>
+      <View style={{ width: 130, height: 5, backgroundColor: '#FF9B38', marginBottom: 20 }}></View>
     </View>
   );
 }
@@ -13,8 +23,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#44A9C9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  }
 });
