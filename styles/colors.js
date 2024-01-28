e29b3695d36ac11d21d973492542d34bdb244f8a
RiwRiwara <awirut2629@gmail.com>
2024-01-28 15:16:11 +0700

    add template font and colors

--- styles/colors.js
+++ styles/colors.js
@@ -1,9 +1,17 @@
 // colors.js
-
 export const COLORS = {
-    primary: '#166085',
-    secondary: '#F27F0C',
-    text: '#333',
-    // Add more colors as needed
-  };
-  
+  NEUTRAL: {
+    900: '#053F5C',
+    
+  },
+
+  PRIMARY: {
+
+  },
+  SECONDARY: {
+
+  },
+  WARNING: {
+
+  },
+};
--- styles/fonts.js
+++ styles/fonts.js
@@ -1,8 +1,35 @@
 // fonts.js
+export const COMMON_ATTRIBUTES = {
+  fontFamily: 'IBMPlexSansThai',
+  fontWeight: 'normal',
+  color: '#fff',
+};
 
 export const FONTS = {
-    regular: 'Roboto-Regular',
-    bold: 'Roboto-Bold',
-    // Add more fonts as needed
-  };
-  
+  TITLE_1: {
+    bold: {
+      ...COMMON_ATTRIBUTES,
+      fontFamily: 'IBMPlexSansThai-Bold',
+      fontSize: 34,
+      fontWeight: 'bold',
+    },
+    regular: {
+      ...COMMON_ATTRIBUTES,
+      fontFamily: 'IBMPlexSansThai-Regular',
+      fontSize: 34,
+    },
+  },
+  TITLE_2: {
+    bold: {
+      ...COMMON_ATTRIBUTES,
+      fontFamily: 'IBMPlexSansThai-Bold',
+      fontSize: 30,
+      fontWeight: 'bold',
+    },
+    regular: {
+      ...COMMON_ATTRIBUTES,
+      fontFamily: 'IBMPlexSansThai-Regular',
+      fontSize: 30,
+    },
+  },
+};
