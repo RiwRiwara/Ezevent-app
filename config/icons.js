4c8566c00c288cdfed5a07ea81112f9fed5a2820
xKollawaTx <93873084+xKollawaTx@users.noreply.github.com>
2024-02-23 18:03:57 +0700

    finish tabs bar (don't use gluestack)

---
+++ config/icons.js
@@ -0,0 +1,17 @@
+const Explore = require("../assets/icons/Explore.png")
+const Inbox = require("../assets/icons/Inbox.png")
+const Profile = require("../assets/icons/Profile.png")
+const ProfileOutline = require("../assets/icons/ProfileOutline.png")
+const MyeventOutline = require("../assets/icons/MyeventOutline.png")
+const qr = require("../assets/icons/qr.png")
+
+const icon = {
+    Explore,
+    Inbox,
+    Profile,
+    ProfileOutline,
+    MyeventOutline,
+    qr
+}
+
+export default icon
