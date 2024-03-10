2031aeb560c74b865e6a7caadfc8d44c9f2dbde4
xKollawaTx <93873084+xKollawaTx@users.noreply.github.com>
2024-03-10 16:47:03 +0700

    add glustack

--- app/compo/login.js
+++ app/compo/login.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import icons from "../../config/icons";
 import loginimage from "../../assets/loginimage.png";
 import { COLORS } from "../../config/color";
-import { GluestackUIProvider, Button, ButtonText } from "@gluestack-ui/themed";
+import {
+  GluestackUIProvider,
+  Button,
+  ButtonText,
+  VStack,
+  Input,
+  InputField,
+  HStack,
+  ButtonIcon,
+} from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 
 const Login = ({ onSubmit }) => {
@@ -19,52 +28,57 @@ const Login = ({ onSubmit }) => {
   return (
     <GluestackUIProvider config={config}>
       <View>
-        <View style={styles.container}>
-          <View style={styles.left}>
-            <Image source={icons.info} style={{ width: 20, height: 20 }} />
-            <Text style={styles.description}> English</Text>
-          </View>
-          <Image source={icons.reject} style={{ width: 20, height: 20 }} />
-        </View>
-        <View style={styles.imageContainer}>
-          <Image source={loginimage} style={styles.image} />
-        </View>
-        <View style={styles.text}>
-          <View style={styles.header}>
-            <Text style={styles.description}>Welcome to </Text>
-            <Text style={styles.title}>Ezevent</Text>
+        <VStack>
+          <HStack space="md" py="$2" style={styles.container}>
+            <HStack>
+              <Image source={icons.info} style={{ width: 20, height: 20 }} />
+              <Text style={styles.description}> English</Text>
+            </HStack>
+            <Image source={icons.reject} style={{ width: 20, height: 20 }} />
+          </HStack>
+          <VStack>
+            <View style={styles.imageContainer}>
+              <Image source={loginimage} style={styles.image} />
+            </View>
+            <View style={styles.text}>
+              <View style={styles.header}>
+                <Text style={styles.description}>Welcome to </Text>
+                <Text style={styles.title}>Ezevent</Text>
+                <Text style={styles.description}>
+                  Application for explore events and manage event{" "}
+                </Text>
+                <Text style={styles.description}>
+                  {" "}
+                  if you need to join please enter mobile number.
+                </Text>
+              </View>
+            </View>
+          </VStack>
+          <VStack space="md" py="$2">
+            <Input borderColor= {COLORS.neutral9}>
+              <InputField
+                style={styles.input}
+                py="$2"
+                placeholder="Email"
+                placeholderTextColor={COLORS.neutral7}
+              />
+            </Input>
+            <Input borderColor= {COLORS.neutral9}>
+              <InputField
+                style={styles.input}
+                py="$2"
+                placeholder="Password"
+                placeholderTextColor={COLORS.neutral7}
+              />
+            </Input>
+            <Button size="sm" bg="#D05F00">
+              <ButtonText>Login</ButtonText>
+            </Button>
             <Text style={styles.description}>
-              Application for explore events and manage event{" "}
+              Don't have an account? register
             </Text>
-            <Text style={styles.description}>
-              {" "}
-              if you need to join please enter mobile number.
-            </Text>
-          </View>
-        </View>
-        <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Username"
-            onChangeText={(text) => setUsername(text)}
-            placeholderTextColor={COLORS.neutral7}
-            value={username}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry={true} // Hide password characters
-            onChangeText={(text) => setPassword(text)}
-            placeholderTextColor={COLORS.neutral7}
-            value={password}
-          />
-          <Button style={styles.button} onPress={handleLogin}>
-            <ButtonText style={styles.button}>Login</ButtonText>
-          </Button>
-          <Text style={styles.description}>
-            Don't have an account? register
-          </Text>
-        </View>
+          </VStack>
+        </VStack>
       </View>
     </GluestackUIProvider>
   );
@@ -99,31 +113,6 @@ const styles = StyleSheet.create({
     width: 200,
     height: 300,
     borderRadius: 10, // Rounded corners
-  },
-  form: {
-    alignItems: "center",
-  },
-  input: {
-    height: 40,
-    borderColor: COLORS.neutral9, // Light border
-    borderWidth: 1,
-    borderRadius: 5, // Rounded corners
-    padding: 10,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: COLORS.primary6, // Blue button
-    color: COLORS.white, // White text
-    padding: 5,
-    borderRadius: 5,
-  },
-  link: {
-    marginTop: 10,
-    color: "#333",
-  },
-  left: {
-    flex: 1,
-    flexDirection: "row",
   },
 });
 
