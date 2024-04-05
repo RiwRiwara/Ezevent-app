5ac789dbc7c8688d28628698fc54c262814efb25
3422 <sirawit.pongsawet@hotmail.com>
2024-04-05 20:42:43 +0700

    SettingPage

---
+++ app/(tabs)/(app)/me/Push_Notification.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Redirect, Stack } from "expo-router";
+import { useSession } from "@providers/ctx";
+import { HStack, Switch, VStack, Text } from "@gluestack-ui/themed";
+import { useTranslation } from "react-i18next";
+
+const Push_Notification = () => {
+
+  const { t } = useTranslation();
+
+  return (
+    <VStack>
+        <HStack
+            justifyContent="space-between"
+            px={15}
+            py={10}
+            height={48}
+            alignItems="center"
+        >
+        <Text color="$primary9">{t('enable_noti')}</Text>
+        <Switch size="md" isDisabled={false} />
+        </HStack>
+        <HStack
+            justifyContent="space-between"
+            px={15}
+            py={10}
+            height={48}
+            alignItems="center"
+        >
+        <Text color="$primary9">{t('send_noti_email')}</Text>
+        <Switch size="md" isDisabled={false} />
+        </HStack>
+        <HStack
+            justifyContent="space-between"
+            px={15}
+            py={10}
+            height={48}
+            alignItems="center"
+        >
+        <Text color="$primary9">{t('send_noti_sms')}</Text>
+        <Switch size="md" isDisabled={false} />
+        </HStack>
+    </VStack>
+
+  );
+};
+
+export default Push_Notification;
